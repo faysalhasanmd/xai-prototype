912fc8898b70bb7f5fc97e5ee0d7350289f5dafe
Md.Faysal Hasan <faysalhasanmd393@mail.com>
2026-07-04 15:20:38 +0600

    add lenis scroll behaviour

---
+++ src/components/LenisProvider/LenisProvider.jsx
@@ -0,0 +1,35 @@
+// components/LenisProvider.jsx
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function LenisProvider({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      touchMultiplier: 1.5,
+    });
+
+    // Lenis-er scroll event ke GSAP ScrollTrigger-er sathe sync koro
+    lenis.on("scroll", ScrollTrigger.update);
+
+    // GSAP-er internal ticker use koro Lenis chalanor jonne
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
